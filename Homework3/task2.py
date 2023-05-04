9a8206e6aad436e4ba71344185e656c0bb09dbcc
ms.chesnokov <ms.chesnokov@tensor.ru>
2023-05-04 09:44:50 +0300

    Поправил замечание

--- Homework3/task2.py
+++ Homework3/task2.py
@@ -7,7 +7,7 @@ def get_list_info(lst):
     min_elem = min(lst)
     max_elem = max(lst)
     sum_list = sum(lst)
-    average = (sum_list / len(lst)).__round__(2)
+    average = round(sum_list / len(lst), 2)
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
