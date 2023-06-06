9b89b5d8086ca5fdf49a4c88a65e72d048187920
chesnokovmike <redxiii41@gmail.com>
2023-06-06 21:49:55 +0300

    Задание 9

--- Homework9/task_1.py
+++ Homework9/task_1.py
@@ -2,10 +2,15 @@
 # Он содержит текст, в словах которого есть цифры.
 # Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
 
-
 # Здесь пишем код
-first_task_data = open('D:\\autotest_course\Homework9\\test_file\\task1_data.txt', 'r', encoding='utf-8')
-with open('D:\\autotest_course\Homework9\\test_file\\task1_answer.txt', 'w', encoding='utf-8') as first_task_answer:
+from pathlib import Path
+
+
+first_task_data_path = Path('test_file/task1_data.txt')
+first_task_answer_path = Path('test_file/task1_answer.txt')
+
+first_task_data = open(first_task_data_path, 'r', encoding='utf-8')
+with open(first_task_answer_path, 'w', encoding='utf-8') as first_task_answer:
     for one_line in first_task_data.readlines():
         for symbol in one_line:
             if symbol.isdigit():
--- Homework9/task_3.py
+++ Homework9/task_3.py
@@ -4,15 +4,21 @@
 # Нужно найти сумму трёх самых дорогих покупок, которые запишутся в переменную three_most_expensive_purchases
 
 # Здесь пишем код
+from pathlib import Path
+
+
 goods_list = []
 tmp_goods = 0
-with open('D:\\autotest_course\Homework9\\test_file\\task_3.txt', 'r') as file:
+task3_file_path = Path('test_file/task_3.txt')
+
+with open(task3_file_path, 'r', encoding='utf-8') as file:
     for goods in file.readlines():
         if str(goods) == '\n':
             goods_list.append(tmp_goods)
             tmp_goods = 0
         else:
             tmp_goods += int(goods)
+
 goods_list.sort(reverse=True)
 three_most_expensive_purchases = goods_list[0] + goods_list[1] + goods_list[2]
 
