d8444bbbf6de32c5e9afd4c607814ad2376c4e35
chesnokovmike <redxiii41@gmail.com>
2023-06-12 21:47:53 +0300

    Задание 11

--- Homework11/task_with_star.py
+++ Homework11/task_with_star.py
@@ -8,4 +8,35 @@
 
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from time import sleep
+
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory': 'D:\\autotest_course\Homework11'}
+chrome_options.add_experimental_option('prefs', prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+try:
+    print('Перейти на  https://sbis.ru/')
+    driver.get('https://sbis.ru/')
+    sleep(1)
+    print('В Footer e найти "Скачать СБИС"')
+    footer_links = driver.find_elements(By.CLASS_NAME, 'sbisru-Footer__link')
+    download_sbis = footer_links[39]
+    print('Перейти по ней')
+    driver.execute_script("return arguments[0].scrollIntoView(true);", download_sbis)
+
+    download_sbis.click()
+    sleep(1)
+    sbis_plugin = driver.find_element(By.CSS_SELECTOR, '[data-id="plugin"]')
+    sbis_plugin.click()
+    sleep(1)
+    download_link = driver.find_element(By.CSS_SELECTOR,
+                                        '[href="https://update.sbis.ru/Sbis3Plugin/master/win32/sbisplugin-setup-web.exe"]')
+    download_link.click()
+
+
+finally:
+    driver.quit()
